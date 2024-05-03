0aa489e29fc930bf25ef869af1b88fffa9fe7f82
Nicola Rubini <nicola.rubini@cern.ch>
2024-05-03 15:48:06 +0200

    start of recodata QA

--- macros/recoQA.C
+++ macros/recoQA.C
@@ -1,29 +1,6 @@
 #include "../lib/lightio.h"
 #include "../lib/data.h"
-
-struct recodata
-{
-    unsigned short n = 0;
-    float x[1024];
-    float y[1024];
-    float t[1024];
-};
-
-void load_data(TTree *reco_data_tree, recodata &target_data_struct)
-{
-    reco_data_tree->SetBranchAddress("n", &target_data_struct.n);
-    reco_data_tree->SetBranchAddress("x", &target_data_struct.x);
-    reco_data_tree->SetBranchAddress("y", &target_data_struct.y);
-    reco_data_tree->SetBranchAddress("t", &target_data_struct.t);
-}
-
-TTree *load_data(std::string filename, recodata &target_data_struct)
-{
-    auto input_file = TFile::Open(filename.c_str());
-    auto input_tree = (TTree *)input_file->Get("recodata");
-    load_data(input_tree, target_data_struct);
-    return input_tree;
-}
+#include "../lib/utility.h"
 
 void recoQA(std::string input_file = "recodata.root", std::string output_file = "out.root", std::string save_dir = "./images/")
 {
