c020ab6215ea8c53850a16bcfeb1afaf9c77be2d
Nicola Rubini <nicola.rubini@cern.ch>
2024-05-03 15:35:00 +0200

    Start of recodata QA

---
+++ macros/recoQA.C
@@ -0,0 +1,7 @@
+#include "../lib/lightio.h"
+#include "../lib/data.h"
+
+void recoQA(std::string input_file = "recodata.root", std::string output_file = "out.root", std::string save_dir = "./images/")
+{
+  
+}
