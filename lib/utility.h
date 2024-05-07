ef08bf68537fc66114bd5e09930147c4b1c4d8ef
Nicola Rubini <nicola.rubini@cern.ch>
2024-05-07 17:23:35 +0200

    Update and fixes

--- lib/utility.h
+++ lib/utility.h
@@ -1,6 +1,8 @@
 #pragma once
 
 //  Data structures
+//  === Fit results: X, Y, R and errors
+using circle_fit_results = std::array<std::array<float, 2>, 3>;
 //  === recodata
 //  === === Structure
 struct recodata
@@ -86,6 +88,17 @@ bool is_within_SiPM(std::array<float, 2> cartesian_coordinates, std::array<float
     is_within_SiPM = true;
   return is_within_SiPM;
 }
+bool is_within_ring(std::array<float, 2> cartesian_coordinates, std::array<float, 3> ring_parameters, float ring_radius_sigma = 0.)
+{
+  //  Improvements:
+  //  Make X and Y have different possible values
+  //  Check values received for the interval
+  bool is_within_ring = false;
+  auto target_radius = cartesian_to_polar(cartesian_coordinates, {ring_parameters[0], ring_parameters[1]})[0];
+  if ((target_radius >= ring_parameters[2] - ring_radius_sigma) && (target_radius <= ring_parameters[2] + ring_radius_sigma))
+    is_within_ring = true;
+  return is_within_ring;
+}
 template <bool rnd_smearing = true>
 void fill_persistance(TH2F *hTarget, recodata reco_data)
 {
@@ -168,6 +181,368 @@ void fill_with_ring_coverage(TH2F *target, std::array<float, 3> ring_parameters,
     }
   }
 }
+//  === Ring finders & fittera
+std::vector<std::tuple<float, float, int>>
+find_peaks(TH1F *original_histo, int bin_span = 3, float cutoff_threshold = 0.33)
+{
+  //  Result
+  std::vector<std::tuple<float, float, int>> result;
+
+  //  Clone target to manipulate
+  auto target_histo = (TH1F *)(original_histo->Clone("tmp_get_filtered_center"));
+  auto target_maximum = target_histo->GetMaximum();
+
+  // Loop over bins
+  for (auto iBin = 1 + bin_span; iBin <= target_histo->GetNbinsX() - bin_span; iBin++)
+  {
+    auto neighbourhood_maximum = 0;
+    auto current_bin = target_histo->GetBinContent(iBin);
+    for (auto iSpan = 1; iSpan <= bin_span; iSpan++)
+    {
+      auto current_bin_left = target_histo->GetBinContent(iBin - iSpan);
+      auto current_bin_right = target_histo->GetBinContent(iBin + iSpan);
+      if ((neighbourhood_maximum < current_bin_left) || (neighbourhood_maximum < current_bin_right))
+        neighbourhood_maximum = max(current_bin_left, current_bin_right);
+      if (neighbourhood_maximum > current_bin)
+        break;
+    }
+    if ((neighbourhood_maximum < current_bin) && (current_bin > cutoff_threshold * target_maximum))
+      result.push_back({target_histo->GetBinCenter(iBin), target_histo->GetBinContent(iBin), iBin});
+  }
+
+  delete target_histo;
+  return result;
+}
+std::vector<std::vector<std::array<float, 2>>>
+find_rings(TH1F *original_histo_X, TH1F *original_histo_Y, float x_center = -1.5, float y_center = -1.5)
+{
+  //  Clone target to manipulate
+  auto target_histo_X = (TH1F *)(original_histo_X->Clone("tmp_find_rings_X"));
+  auto target_histo_Y = (TH1F *)(original_histo_Y->Clone("tmp_find_rings_Y"));
+
+  //  Find peaks
+  auto current_peaks_X = find_peaks(target_histo_X);
+  auto current_peaks_Y = find_peaks(target_histo_Y);
+
+  //  ==  Assumption is made that the beam is within the smaller circle
+  auto n_cirles = 0;
+  auto current_circle = 0;
+  std::vector<std::vector<std::array<float, 2>>> circles;
+
+  //  First loop on x_findings
+  for (auto current_peak : current_peaks_X)
+  {
+    auto current_X = get<0>(current_peak);
+    if (current_X < 0)
+    {
+      n_cirles++;
+      circles.push_back({{current_X, y_center}});
+      current_circle++;
+    }
+    else
+    {
+      current_circle--;
+      if (current_circle < 0)
+      {
+        current_circle = n_cirles;
+        n_cirles++;
+        circles.insert(circles.begin(), {{current_X, y_center}});
+        continue;
+      }
+      circles[current_circle].push_back({current_X, y_center});
+    }
+  }
+
+  //  Second loop on y_findings
+  auto n_left_rings = 0;
+  for (auto current_peak : current_peaks_Y)
+  {
+    auto current_Y = get<0>(current_peak);
+    if (current_Y < 0)
+      n_left_rings++;
+  }
+  if (n_left_rings == n_cirles)
+  {
+    current_circle = 0;
+    for (auto current_peak : current_peaks_Y)
+    {
+      auto current_Y = get<0>(current_peak);
+      if (current_Y < 0)
+      {
+        circles[current_circle].push_back({x_center, current_Y});
+        current_circle++;
+      }
+      else
+      {
+        current_circle--;
+        if (current_circle < 0)
+        {
+          current_circle = n_cirles;
+          n_cirles++;
+          circles.insert(circles.begin(), {{x_center, current_Y}});
+          continue;
+        }
+        circles[current_circle].push_back({x_center, current_Y});
+      }
+    }
+  }
+
+  delete target_histo_X;
+  delete target_histo_Y;
+  std::reverse(circles.begin(), circles.end());
+  return circles;
+}
+std::vector<std::vector<std::array<float, 2>>>
+merge_circles(std::vector<std::vector<std::vector<std::array<float, 2>>>> target_circles)
+{
+  std::vector<std::vector<std::array<float, 2>>> final_circles;
+  for (auto current_circle_array : target_circles)
+  {
+    auto icircle = -1;
+    for (auto current_circle : current_circle_array)
+    {
+      icircle++;
+      if (final_circles.size() < icircle + 1)
+        final_circles.push_back({});
+      for (auto current_point : current_circle)
+      {
+        final_circles[icircle].push_back(current_point);
+      }
+    }
+  }
+  return final_circles;
+}
+template <bool fix_XY = true>
+circle_fit_results
+fit_circle(TGraph *gTarget, std::array<float, 3> initial_values)
+{
+  circle_fit_results result;
+
+  //  Chi2 minimisation for points in a circle
+  auto chi2_function = [&](const double *parameters)
+  {
+    float chi2 = 0;
+    for (int iPnt = 0; iPnt < gTarget->GetN(); iPnt++)
+    {
+      double delta_x = gTarget->GetPointX(iPnt) - parameters[0];
+      double delta_y = gTarget->GetPointY(iPnt) - parameters[1];
+      double delta_r = parameters[2] - std::sqrt(delta_x * delta_x + delta_y * delta_y);
+      chi2 += delta_r * delta_r;
+    }
+    return chi2;
+  };
+
+  // wrap chi2 function in a function object for the fit
+  ROOT::Math::Functor fit_function(chi2_function, 3);
+  ROOT::Fit::Fitter fitter;
+
+  //  Set initial values and variables names
+  double internal_initial_values[3] = {initial_values[0], initial_values[1], initial_values[2]};
+  fitter.SetFCN(fit_function, internal_initial_values);
+  fitter.Config().ParSettings(0).SetName("x0");
+  fitter.Config().ParSettings(1).SetName("y0");
+  fitter.Config().ParSettings(2).SetName("R");
+  fitter.Config().ParSettings(2).SetLowerLimit(0);
+  if (fix_XY)
+  {
+    fitter.Config().ParSettings(0).Fix();
+    fitter.Config().ParSettings(1).Fix();
+  }
+
+  //  Fitting
+  if (!fitter.FitFCN())
+  {
+    // Error("fit_circle", "Fit failed");
+    //  return {{{-2., 0.}, {-2., 0.}, {-2., 0.}}};
+  }
+  const ROOT::Fit::FitResult &fit_result = fitter.Result();
+
+  auto iTer = -1;
+  for (auto current_parameter : fit_result.Parameters())
+  {
+    iTer++;
+    result[iTer][0] = current_parameter;
+    result[iTer][1] = fit_result.Errors()[iTer];
+  }
+
+  //  Calculate chi2
+  double *test = new double[3];
+  test[0] = result[0][0];
+  test[1] = result[1][0];
+  test[2] = result[2][0];
+  auto myChi2 = chi2_function(test);
+
+  return result;
+}
+template <bool fix_XY = true>
+circle_fit_results
+fit_circles(std::vector<TGraphErrors *> gTargets, std::array<float, 2> center_guess, std::vector<float> radiuses_guesses)
+{
+  //   Result
+  circle_fit_results result;
+
+  if (gTargets.size() == 0)
+    return result;
+
+  if (gTargets.size() == 1)
+    return fit_circle<false>(gTargets[0], {center_guess[0], center_guess[1], radiuses_guesses[0]});
+
+  //  Chi2 minimisation for points in a circle
+  auto chi2_function = [&](const double *parameters)
+  {
+    float chi2 = 0;
+    auto iGraph = -1;
+    for (auto current_graph : gTargets)
+    {
+      iGraph++;
+      for (int iPnt = 0; iPnt < current_graph->GetN(); iPnt++)
+      {
+        double delta_x = current_graph->GetPointX(iPnt) - parameters[0];
+        double delta_y = current_graph->GetPointY(iPnt) - parameters[1];
+        double delta_r = parameters[2 + iGraph] - std::sqrt(delta_x * delta_x + delta_y * delta_y);
+        chi2 += delta_r * delta_r;
+      }
+    }
+    return chi2;
+  };
+
+  // wrap chi2 function in a function object for the fit
+  ROOT::Math::Functor fit_function(chi2_function, 2 + gTargets.size());
+  ROOT::Fit::Fitter fitter;
+
+  //  Set initial values and variables names
+  double *internal_initial_values = new double[2 + gTargets.size()];
+  internal_initial_values[0] = center_guess[0];
+  internal_initial_values[1] = center_guess[1];
+  auto iTer = 1;
+  for (auto current_radius : radiuses_guesses)
+  {
+    iTer++;
+    if (iTer > (2 + gTargets.size()))
+      break;
+    internal_initial_values[iTer] = current_radius;
+  }
+  fitter.SetFCN(fit_function, internal_initial_values);
+  fitter.Config().ParSettings(0).SetName("x0");
+  fitter.Config().ParSettings(1).SetName("y0");
+
+  iTer = -1;
+  for (auto current_radius : radiuses_guesses)
+  {
+    iTer++;
+    if (iTer >= gTargets.size())
+      break;
+    fitter.Config().ParSettings(2 + iTer).SetName(Form("R_%i", iTer));
+    fitter.Config().ParSettings(2 + iTer).SetLowerLimit(0);
+  }
+  if (fix_XY)
+  {
+    fitter.Config().ParSettings(0).Fix();
+    fitter.Config().ParSettings(1).Fix();
+  }
+
+  //  Fitting
+  if (!fitter.FitFCN())
+  {
+    // Error("fit_circle", "Fit failed");
+    //  return {{{-2., 0.}, {-2., 0.}, {-2., 0.}}};
+  }
+  const ROOT::Fit::FitResult &fit_result = fitter.Result();
+
+  iTer = -1;
+  for (auto current_parameter : fit_result.Parameters())
+  {
+    iTer++;
+    result[iTer][0] = current_parameter;
+    result[iTer][1] = fit_result.Errors()[iTer];
+  }
+  return result;
+}
+template <bool simultaneous_fit = true>
+std::vector<circle_fit_results> fit_multiple_rings(std::vector<std::array<TH1F *, 2>> original_histo_s, std::vector<std::array<float, 2>> centers)
+{
+  std::vector<std::vector<std::vector<std::array<float, 2>>>> final_circles_array;
+  auto iTer = -1;
+  for (auto current_xy_pair : original_histo_s)
+  {
+    iTer++;
+    final_circles_array.push_back(find_rings(current_xy_pair[0], current_xy_pair[1], centers[iTer][0], centers[iTer][1]));
+  }
+
+  auto final_circles = merge_circles(final_circles_array);
+
+  std::vector<circle_fit_results> final_rings;
+  std::vector<TGraphErrors *> gfinal_rings;
+  auto current_n_circle = -1;
+  for (auto current_circle : final_circles)
+  {
+    current_n_circle++;
+    gfinal_rings.push_back(new TGraphErrors());
+    for (auto current_point : current_circle)
+    {
+      gfinal_rings[current_n_circle]->SetPoint(gfinal_rings[current_n_circle]->GetN(), current_point[0], current_point[1]);
+    }
+    auto circle_fit = fit_circle<false>(gfinal_rings[current_n_circle], {0, 0, 20});
+    final_rings.push_back(circle_fit);
+  }
+
+  if (simultaneous_fit)
+  {
+    auto fit_results = fit_circles<false>(gfinal_rings, {0, 0}, {100, 100, 100, 100, 100});
+    auto iRing = -1;
+    for (auto &current_ring : final_rings)
+    {
+      iRing++;
+      current_ring[0][0] = fit_results[0][0];
+      current_ring[1][0] = fit_results[1][0];
+      current_ring[0][1] = fit_results[0][1];
+      current_ring[1][1] = fit_results[1][1];
+      current_ring[2][0] = fit_results[2 + iRing][0];
+      current_ring[2][1] = fit_results[2 + iRing][1];
+    }
+  }
+
+  return final_rings;
+}
+template <bool simultaneous_fit = true>
+std::vector<circle_fit_results> fit_multiple_rings(TH2F *persistance_2D, std::vector<std::array<float, 2>> slices = {{-0., +0.}, {-12., -12.}, {+12., +12.}})
+{
+  //  Clone target to manipulate
+  auto target_persistance_2D = (TH2F *)(persistance_2D->Clone("target_persistance_2D"));
+
+  //  Define utility functions
+  std::vector<std::array<TH1F *, 2>> original_histo_s;
+  std::vector<std::array<float, 2>> centers;
+
+  //  Iteration for requested X-Y slices
+  auto iTer = -1;
+  for (auto current_limits : slices)
+  {
+    iTer++;
+
+    auto x_target_bin = target_persistance_2D->GetXaxis()->FindBin(current_limits[0]);
+    auto y_target_bin = target_persistance_2D->GetXaxis()->FindBin(current_limits[1]);
+
+    auto x_target_bin_center = target_persistance_2D->GetXaxis()->GetBinCenter(x_target_bin);
+    auto y_target_bin_center = target_persistance_2D->GetYaxis()->GetBinCenter(y_target_bin);
+
+    auto hXslice = (TH1F *)(target_persistance_2D->ProjectionX(Form("current_projection_X_%i", iTer), x_target_bin, x_target_bin));
+    auto hYslice = (TH1F *)(target_persistance_2D->ProjectionY(Form("current_projection_Y_%i", iTer), y_target_bin, y_target_bin));
+
+    original_histo_s.push_back({hXslice, hYslice});
+    centers.push_back({(float)(x_target_bin_center), (float)(y_target_bin_center)});
+  }
+
+  auto result = fit_multiple_rings<simultaneous_fit>(original_histo_s, centers);
+  for (auto current_histos : original_histo_s)
+  {
+    delete current_histos[0];
+    delete current_histos[1];
+  }
+  delete target_persistance_2D;
+
+  return result;
+}
 //  === Graphical helpers
 TCanvas *get_std_canvas()
 {
@@ -178,4 +553,86 @@ TCanvas *get_std_canvas()
   result->SetLeftMargin(0.15);
   result->SetBottomMargin(0.15);
   return result;
+}
+template <bool grid_x = true, bool grid_y = true>
+TCanvas *get_std_canvas_2D(std::string name, std::string title, float nXpixels = 1145, float nYpixels = 1000)
+{
+  TCanvas *cResult = new TCanvas(name.c_str(), title.c_str(), nXpixels, nYpixels);
+  gStyle->SetOptStat(0);
+  gPad->SetGridx(grid_x);
+  gPad->SetGridy(grid_y);
+  gPad->SetRightMargin(0.15);
+  gPad->SetTopMargin(0.05);
+  gPad->SetLeftMargin(0.15);
+  gPad->SetBottomMargin(0.15);
+  return cResult;
+}
+void plot_box(std::array<float, 4> parameters, int line_color = kBlack, int line_style = kSolid, int line_width = 1)
+{
+  auto result = new TBox(parameters[0], parameters[1], parameters[2], parameters[3]);
+  result->SetFillStyle(0);
+  result->SetLineColor(line_color);
+  result->SetLineStyle(line_style);
+  result->SetLineWidth(line_width);
+  result->DrawBox(parameters[0], parameters[1], parameters[2], parameters[3]);
+}
+void plot_circle(std::array<float, 3> parameters, int line_color = kBlack, int line_style = kSolid, int line_width = 1)
+{
+  auto result = new TEllipse(parameters[0], parameters[1], parameters[2]);
+  result->SetFillStyle(0);
+  result->SetLineColor(line_color);
+  result->SetLineStyle(line_style);
+  result->SetLineWidth(line_width);
+  result->DrawEllipse(parameters[0], parameters[1], parameters[2], 0, 0, 360, 0, "same");
+}
+void plot_circle(std::array<std::array<float, 2>, 3> parameters, std::array<std::array<int, 3>, 3> plot_options)
+{
+  std::array<float, 3> reduced_parameters = {parameters[0][0], parameters[1][0], parameters[2][0]};
+  plot_circle(reduced_parameters, plot_options[0][0], plot_options[0][1], plot_options[0][2]);
+  reduced_parameters = {parameters[0][0], parameters[1][0], parameters[2][0] + parameters[2][1]};
+  plot_circle(reduced_parameters, plot_options[1][0], plot_options[1][1], plot_options[1][2]);
+  reduced_parameters = {parameters[0][0], parameters[1][0], parameters[2][0] - parameters[2][1]};
+  plot_circle(reduced_parameters, plot_options[1][0], plot_options[1][1], plot_options[1][2]);
+  std::array<float, 4> box_parameters = {parameters[0][0] - parameters[0][1], parameters[1][0] - parameters[1][1], parameters[0][0] + parameters[0][1], parameters[1][0] + parameters[1][1]};
+  plot_box(box_parameters, plot_options[2][0], plot_options[2][1], plot_options[2][2]);
+}
+TCanvas *plot_check_coordinates(TH2F *hPersistance2D, std::vector<circle_fit_results> found_rings)
+{
+  TCanvas *cDrawResult = get_std_canvas_2D("cDrawResult", "cDrawResult", 1145 * 2, 1000);
+  cDrawResult->Divide(2, 1);
+  TLatex *lLatex = new TLatex();
+  cDrawResult->cd(2);
+  gPad->SetRightMargin(0.15);
+  gPad->SetTopMargin(0.05);
+  gPad->SetLeftMargin(0.15);
+  gPad->SetBottomMargin(0.15);
+  cDrawResult->cd(1);
+  gPad->SetRightMargin(0.15);
+  gPad->SetTopMargin(0.05);
+  gPad->SetLeftMargin(0.15);
+  gPad->SetBottomMargin(0.15);
+  hPersistance2D->Draw("COLZ");
+  auto iRing = -1;
+  for (auto current_ring : found_rings)
+  {
+    iRing++;
+
+    cDrawResult->cd(1);
+    std::array<std::array<int, 3>, 3> plot_options = {{{kBlack, kSolid, 3}, {kRed, kDashed, 2}, {kRed, kSolid, 2}}};
+    std::array<std::array<float, 2>, 3> plot_coordinates = {{{current_ring[0][0], current_ring[0][1]}, {current_ring[1][0], current_ring[1][1]}, {current_ring[2][0], current_ring[2][1]}}};
+    plot_circle(plot_coordinates, plot_options);
+
+    cDrawResult->cd(2);
+
+    lLatex->SetTextSize(0.045);
+    lLatex->DrawLatexNDC(0.02, 0.95 + iRing * 0.25, Form("Ring %i", iRing));
+
+    lLatex->SetTextSize(0.03);
+    lLatex->DrawLatexNDC(0.02, 0.91 + iRing * 0.25, Form("Guess (mm):"));
+    lLatex->DrawLatexNDC(0.18, 0.91 + iRing * 0.25, Form("X_{0} : %.2f#pm%.2f", current_ring[0][0], current_ring[0][1]));
+    lLatex->DrawLatexNDC(0.36, 0.91 + iRing * 0.25, Form("Y_{0} : %.2f#pm%.2f", current_ring[1][0], current_ring[1][1]));
+    lLatex->DrawLatexNDC(0.54, 0.91 + iRing * 0.25, Form("R_{0} : %.2f#pm%.2f", current_ring[2][0], current_ring[2][1]));
+    lLatex->DrawLatexNDC(0.72, 0.91 + iRing * 0.25, Form("#sigma_{R} : %.2f#pm%.2f", 0., 0.));
+  }
+  return cDrawResult;
 }
--- macros/recoQA.C
+++ macros/recoQA.C
@@ -3,76 +3,86 @@
 #include "../lib/mapping.h"
 #include "../lib/utility.h"
 
-void recoQA(std::string input_file = "recodata.root", std::string output_file = "out.root", std::string save_dir = "./images/")
+void recoQA(std::string input_file = "recodata_2.root", std::string output_file = "out.root", std::string save_dir = "./images/")
 {
-    //  Output
-    auto hPersistance2D = new TH2F("hPersistance2D", ";X (mm);Y (mm); t (ns)", 396, -99, 99, 396, -99, 99);
-    auto hMap_fullsetup_SiPM = new TH2F("hMap_fullsetup_SiPM", ";X (mm);Y (mm)", 4000, -100, 100, 4000, -100, 100);
-    auto hMap_availsetup_SiPM = new TH2F("hMap_availsetup_SiPM", ";X (mm);Y (mm)", 4000, -100, 100, 4000, -100, 100);
-    std::vector<TH2F*> hMap_found_Rings;
+  //  Output
+  auto hPersistance2D = new TH2F("hPersistance2D", ";X (mm);Y (mm); t (ns)", 396, -99, 99, 396, -99, 99);
+  auto hPersistance2D_initial_guess = new TH2F("hPersistance2D_initial_guess", ";X (mm);Y (mm); t (ns)", 66, -99, 99, 66, -99, 99);
+  auto hMap_fullsetup_SiPM = new TH2F("hMap_fullsetup_SiPM", ";X (mm);Y (mm)", 4000, -100, 100, 4000, -100, 100);
+  auto hMap_availsetup_SiPM = new TH2F("hMap_availsetup_SiPM", ";X (mm);Y (mm)", 4000, -100, 100, 4000, -100, 100);
+  std::vector<TH2F *> hMap_found_Rings;
 
-    //  Link TTree to local data instance
-    recodata reco_data;
-    auto reco_tree = load_data(input_file, reco_data);
+  //  Link TTree to local data instance
+  recodata reco_data;
+  auto reco_tree = load_data(input_file, reco_data);
 
-    //  Store available SiPMs
-    std::vector<std::array<float, 2>> list_of_available_SiPMs;
+  //  Store available SiPMs
+  std::vector<std::array<float, 2>> list_of_available_SiPMs;
 
-    //  First loop on events
-    cout << "[INFO] Start of preliminary loop for X_{0}, Y_{0} and R_{0}" << endl;
-    for (int iEv = 0; iEv < reco_tree->GetEntries(); iEv++)
+  //  First loop on events
+  cout << "[INFO] Start of preliminary loop for X_{0}, Y_{0} and R_{0}" << endl;
+  for (int iEv = 0; iEv < reco_tree->GetEntries(); iEv++)
+  {
+    //  Recover recodata entry form tree
+    reco_tree->GetEntry(iEv);
+
+    if ((iEv+1) % 10000 == 0)
     {
-        //  Recover recodata entry form tree
-        reco_tree->GetEntry(iEv);
-
-        if (iEv % 1000 == 0)
-            cout << "[INFO] event: " << iEv << endl;
-
-        //  Persistance plot
-        fill_persistance(hPersistance2D, reco_data);
-
-        //  Loop on hits
-        for (int iPnt = 0; iPnt < reco_data.n; iPnt++)
-        {
-            if (!count(list_of_available_SiPMs.begin(), list_of_available_SiPMs.end(), std::array<float, 2>({reco_data.x[iPnt], reco_data.y[iPnt]})))
-                //  Add sensor sensitive area
-                list_of_available_SiPMs.push_back({reco_data.x[iPnt], reco_data.y[iPnt]});
-        }
+      cout << "[INFO] event: " << iEv << endl;
+      break;
     }
-    for (auto iPDU = 0; iPDU < 8; iPDU++)
-        for (auto iCol = 0; iCol < 16; iCol++)
-            for (auto iRow = 0; iRow < 16; iRow++)
-                fill_with_SiPM_coverage(hMap_fullsetup_SiPM, sipm4eic::get_position({iPDU, iCol, iRow}));
-    for (auto current_position : list_of_available_SiPMs)
-        fill_with_SiPM_coverage(hMap_availsetup_SiPM, current_position);
 
-    //  === Graphics
-    gROOT->SetBatch();
-    system(Form("mkdir -p %s/", save_dir.c_str()));
+    //  Persistance plot
+    fill_persistance(hPersistance2D, reco_data);
+    fill_persistance<false>(hPersistance2D_initial_guess, reco_data);
 
-    gStyle->SetPalette(kInvertedDarkBodyRadiator);
+    //  Loop on hits
+    for (int iPnt = 0; iPnt < reco_data.n; iPnt++)
+    {
+      if (!count(list_of_available_SiPMs.begin(), list_of_available_SiPMs.end(), std::array<float, 2>({reco_data.x[iPnt], reco_data.y[iPnt]})))
+        //  Add sensor sensitive area
+        list_of_available_SiPMs.push_back({reco_data.x[iPnt], reco_data.y[iPnt]});
+    }
+  }
+  /*
+  for (auto iPDU = 0; iPDU < 8; iPDU++)
+      for (auto iCol = 0; iCol < 16; iCol++)
+          for (auto iRow = 0; iRow < 16; iRow++)
+              fill_with_SiPM_coverage(hMap_fullsetup_SiPM, sipm4eic::get_position({iPDU, iCol, iRow}));
+  for (auto current_position : list_of_available_SiPMs)
+      fill_with_SiPM_coverage(hMap_availsetup_SiPM, current_position);
+*/
+  auto found_rings = fit_multiple_rings(hPersistance2D_initial_guess);
 
-    //  === === Persistance
-    auto current_canvas = get_std_canvas();
-    hPersistance2D->Draw();
-    current_canvas->SaveAs(Form("%s/hPersistance2D.png", save_dir.c_str()));
+  //  === Graphics
+  gROOT->SetBatch();
+  system(Form("mkdir -p %s/", save_dir.c_str()));
 
-    //  === === SiPM and ring coverage
-    //  === === === All SiPMs
-    current_canvas = get_std_canvas();
-    hMap_fullsetup_SiPM->Draw();
-    current_canvas->SaveAs(Form("%s/hMap_fullsetup_SiPM.png", save_dir.c_str()));
-    //  === === === Available SiPMs
-    current_canvas = get_std_canvas();
-    hMap_availsetup_SiPM->Draw();
-    current_canvas->SaveAs(Form("%s/hMap_availsetup_SiPM.png", save_dir.c_str()));
-    //  === === === Rings coverage
+  gStyle->SetPalette(kInvertedDarkBodyRadiator);
 
-    TFile *out = new TFile(output_file.c_str(), "RECREATE");
-    hPersistance2D->Write();
-    hMap_fullsetup_SiPM->Write();
-    hMap_availsetup_SiPM->Write();
-    out->Close();
+  //  === === Persistance
+  auto current_canvas = get_std_canvas();
+  hPersistance2D->Draw();
+  current_canvas->SaveAs(Form("%s/hPersistance2D.png", save_dir.c_str()));
 
-    gROOT->SetBatch(kFALSE);
+  //  === === SiPM and ring coverage
+  //  === === === All SiPMs
+  current_canvas = get_std_canvas();
+  hMap_fullsetup_SiPM->Draw();
+  current_canvas->SaveAs(Form("%s/hMap_fullsetup_SiPM.png", save_dir.c_str()));
+  //  === === === Available SiPMs
+  current_canvas = get_std_canvas();
+  hMap_availsetup_SiPM->Draw();
+  current_canvas->SaveAs(Form("%s/hMap_availsetup_SiPM.png", save_dir.c_str()));
+  //  === === === Rings coverage
+  current_canvas = plot_check_coordinates(hPersistance2D, found_rings);
+  current_canvas->SaveAs(Form("%s/plot_check_coordinates.png", save_dir.c_str()));
+
+  TFile *out = new TFile(output_file.c_str(), "RECREATE");
+  hPersistance2D->Write();
+  hMap_fullsetup_SiPM->Write();
+  hMap_availsetup_SiPM->Write();
+  out->Close();
+
+  gROOT->SetBatch(kFALSE);
 }
